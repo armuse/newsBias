52d86ffee941883cc0a23c5f5a921881d1421741
amontalb <alyssa.montalbano@cern.ch>
2020-06-17 22:52:16 -0700

    else ifs in formatting

--- formatting.sh
+++ formatting.sh
@@ -7,7 +7,6 @@ filename="ProQuestDocuments-2020-06-18.txt"
 outputFile="condensedNews.csv"
 
 Title #this is positional, not described by x:
-
 abs
 date
 text
@@ -18,21 +17,17 @@ while read line; # is a for loop better?
 do
   if [[ $line == *"Abstract:"* ]]; then
     abs="${line:9}"
-  fi
 
-  if [[ $line == *"Publication Date:"* ]]; then
+  elif [[ $line == *"Publication Date:"* ]]; then
     date="${line:17}"
-  fi
 
-  if [[ $line == *"Full text:"* ]]; then
+  elif [[ $line == *"Full text:"* ]]; then
     text="${line:10}"
-  fi
 
-  if [[ $line == *"Subject:"* ]]; then
+  elif [[ $line == *"Subject:"* ]]; then
     sub="${line:8}"
-  fi
 
-  if [[ $line == *"ProQuest document ID:"* ]]; then
+  elif [[ $line == *"ProQuest document ID:"* ]]; then
     id="${line:21}"
   fi
 
