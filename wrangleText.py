a61e35a246dc5541843558daba9b14d0387529b9
amontalb <alyssa.montalbano@cern.ch>
2020-06-26 00:11:39 -0700

    -not working- tokenizing words, need to lemmatize

--- wrangleText.py
+++ wrangleText.py
@@ -1,39 +1,59 @@
-import pandas as pd
 import string
 from nltk.tokenize import word_tokenize #splits words and punctuation
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 
-articles = pd.read_csv('metadata.csv')
-articles = articles.drop(columns = ['copyright','Authors','ArticleType','companies','documentType','language','languageOfSummary','placeOfPublication','entryDate','year','startPage','Database'])
-articles.head()
+lemmatizer = WordNetLemmatizer()
 
-#lower title and subjectTerms
-articles['Title'].str.lower()
-articles['subjectTerms'].str.lower()
+inFiles = open('input.txt','r') #ls data/X/* > input.xt
 
-#Add boolean for crime-topic or not
-articles['sentiment'] = pd.Series(0) #0 for netural, 1 for crime etc related 
+neg = ['criminal','firearms','murder','manslaughter','arrests','burglary','robbery','violence','police']
+#victimization, missing persons,
+outDir = 'data/Baldwin-edited'
 
-#this removes numbers too - what about COVID-19?
-articles['Title'] = articles['Title']
+stop_words = set(stopwords.words('english'))
 
-print(articles)
+for article in inFiles: #each article is its own line
+    file = open(article[:-1],'r') #there's \n at end of line
+    keep = True
+    truthNeg = False
+    edited = []
 
-# articles['sentiment'] = articles.apply(lambda row: row[''])
+    for line in file:
+        line = line.lower()
+        if 'alec' in line: #Alec Baldwin is NOISE
+            keep = False
+            continue
+        if line[0:3] == 'sub': #check subject terms for truth classification
+            print(line)
+            for term in neg:
+                if term in line:
+                    truthNeg = True
+                    continue #one true, all true
+        #save only title, full text and subject
+        elif (line[0:6] == 'credit' or line[0:4] == 'illu'
+            or line[0:6] == 'author' or line[0:5] == 'https'
+            or line[0:11] == 'publication'):
+            continue #skip non article information
+        word_tokens = word_tokenize(line)
+        no_punctuation = [word.isalpha() for word in word_tokens]
+        remove_stop = [word for word in no_punctuation if not word in stop_words]
+        lemmed_words = [lemmatizer.lemmatize(word) for word in remove_stop]
+        #remove punctuation
+        #remove stop words
+        #stem/lemm
+        print (lemmed_words)
+        #edited.append(+' ')
 
-#separate word by space
-#word_tokens = word_tokenize(input)
-#print(word_tokens)
-#get stop words
-#stop_words = set(stopwords.words('english'))
-#remove punctuation
-#no_punctuation = [word.lower() for word in word_tokens if word not in punctuations ] #.isalpha(), .isalnum() but hypens removed
-#remove stop words
-#stripped_article = [word for word in no_punctuation if not word in stop_words]
-#remove 'alec'
-#lemmaziation
-#stemming
+    if keep:
+        outName = outDir+article[12:-5]+'-'+str(truthNeg)+'.txt'
+        outFile = open(outName,'w')
+        outFile.write(lemmed_words)
+#        for i in range(len(edited)):
+#            outFile.write(edited[i])
+        outFile.close()
+    file.close()
 
-#print remaining:
-#print(stripped_article)
+    #finally:
+inFiles.close()
